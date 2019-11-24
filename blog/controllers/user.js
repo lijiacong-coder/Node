adf354e9a8e05d3c639f1a0bf5e2f7f1d81c69ff
lijiacong-coder <1143596375@qq.com>
2019-11-24 16:40:05 +0800

    博客

---
+++ blog/controllers/user.js
@@ -0,0 +1,8 @@
+// 注册功能 reg方法  -- - 把views下的reg.ejs加载到浏览器界面
+exports.reg = function(req,res,next){
+    res.render('reg');
+}
+
+exports.do_reg = function(req,res,next){
+    
+}
--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../controllers/user.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/reg',User.reg);
 
 module.exports = router;
